36066e5a757f3ba6715d7a12c9e4e8deb26c7fe4
Duncan <duncanmarten@yahoo.com>
2020-10-28 09:05:32 -0500

    added markdown to the generateMarkdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+    # ${data.title}
+    ## Description
 
-`;
+
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## Installation
+
+
+    ## Usage
+
+
+    ## License
+
+
+    ## Contributing
+
+
+    ## Tests
+
+    
+    ## Questions
+
+  `;
 }
 
 module.exports = generateMarkdown;
